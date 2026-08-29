25ca8a169f93f07d082d7d17ee6fc8e4264ac6c9
Davyn <davyn.miller@hes-so.ch>
2026-08-30 00:35:05 +0300

    Exercise5, 1st controller /index + model book

---
+++ src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -0,0 +1,19 @@
+package fi.haagahelia.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class BookController {
+
+    @GetMapping("/index")
+    public String bookList() {
+        return "index";
+    }
+    
+
+
+
+    
+}
