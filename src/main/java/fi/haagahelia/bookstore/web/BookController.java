649afc7ef46771721f0a19ae97116046ba9d89ec
Davyn <davyn.miller@hes-so.ch>
2026-09-11 15:27:34 +0300

    Chapter 3.3 delete

--- src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import fi.haagahelia.bookstore.repository.BookRepository;
@@ -19,6 +20,13 @@ public class BookController {
     @Autowired 
     private BookRepository bookRepository;
 
+
+    @RequestMapping(value = "/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+        bookRepository.deleteById(bookId);
+        return "redirect:../booklist";
+    }
+    
     @RequestMapping("/add")
     public String addBook(Model model) {
         model.addAttribute("book", new Book());
