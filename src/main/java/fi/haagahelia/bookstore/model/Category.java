5b7b48dae369eeda9e5cc6bf27829a070da35a20
Davyn <davyn.miller@hes-so.ch>
2026-09-24 17:00:57 +0300

    remove useless import

--- src/main/java/fi/haagahelia/bookstore/model/Category.java
+++ src/main/java/fi/haagahelia/bookstore/model/Category.java
@@ -7,7 +7,6 @@ import jakarta.persistence.OneToMany;
 import java.util.List;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity 
